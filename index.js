ada090f13588a4e1d502d341e9ebed8136f29ded
Jesse Roman <jessero912@gmail.com>
2022-03-05 17:02:26 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+    let newArray = []; 
+    for(let i = 0; i < names.length; i++){
+     newArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)   
+ }
+
+return newArray
+
+}
+
+
+
+
+function countDown(thisNumber) {
+while (thisNumber >= 0) {
+    console.log(thisNumber);
+    thisNumber--;
+
+}
+
+}
